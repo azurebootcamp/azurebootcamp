a5d6515511004754ca9a68fc5e4e7011e6f1fe17
Punit Ganshani <punit.ganshani@gmail.com>
2015-12-25 00:37:07 +0800

    Data directory and restructuring of solution
    Data directory (for multiple locations) and restructuring of solution

---
+++ 2016/code/src/bootcamp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+using bootcamp.Models;
+using System.Net;
+
+namespace bootcamp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string location)
+        {
+            LocationInfo locationInfo = null;
+
+            try {
+                if (!string.IsNullOrEmpty(location))
+                {
+                    var url =
+                        String.Format("https://github.com/punitganshani/azurebootcamp/raw/master/2016/data/locations/{0}/data.json", location.ToLowerInvariant());
+                    string contents;
+                    using (var wc = new System.Net.WebClient())
+                        contents = wc.DownloadString(url);
+
+                    locationInfo = JsonConvert.DeserializeObject<LocationInfo>(contents);
+                }
+            }
+            catch(WebException ex) // some problem getting the file
+            {
+                locationInfo = null; //TODO: need to give a default value instead of NULL
+            }
+
+            return View(locationInfo);
+
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2016/code/src/bootcamp/Models/Location.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bootcamp.Models
+{
+    public class LocationInfo
+    {
+        public Location Location { get; set; }
+        public Social Social { get; set; }
+        public Registration Registration { get; set; }
+        public Contact[] Contact { get; set; }
+        public Sponsor[] Sponsors { get; set; }
+        public Prize[] Prizes { get; set; }
+        public Track[]   Agenda { get; set; }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+        public string Address { get; set; }
+    } 
+
+    public class Registration
+    {
+        public string Status { get; set; }
+        public string Link { get; set; }
+    } 
+    public class Track
+    {
+        public string Name { get; set; }
+        public Session[] Sessions { get; set; }
+    }
+
+    public class Session
+    {
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public Speaker Speaker { get; set; }
+    }
+
+    public class Speaker
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public Social Social { get; set; }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public Social Social { get; set; }
+    } 
+
+    public class Sponsor
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Link { get; set; }
+    }
+
+    public class Prize
+    {
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+
+
+
+    public class Social
+    {
+        public string Twitter { get; set; }
+        public string Meetup { get; set; }
+        public string Facebook { get; set; }
+        public string Google { get; set; }
+        public string Website { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
